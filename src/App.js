e41fcef5bc1589a6829d5bcda561ea791a288cb5
SakibArar <sakib.cghs@gmail.com>
2021-04-23 22:15:06 +0600

    Added ffmpeg module and our app loads it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {createFFmpeg, fetchFile} from "@ffmpeg/ffmpeg";
+import { useEffect, useState } from 'react';
+const ffmpeg = createFFmpeg({log:true}); // to see everylog on the console
+
 function App() {
-  return (
+
+  const [ready, setReady] = useState(false);
+
+  const load  = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  return ready?(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
-  );
+  ): (<p>Loading...</p>);
 }
 
 export default App;
